efbf18487d22b23ebb49df39290751bfccdd6abe
daviskyLV <ledaviskylv@gmail.com>
2023-07-05 23:02:19 +0200

    globals and menu changes

--- Globals.gd
+++ Globals.gd
@@ -1,7 +1,10 @@
 extends Node
 
-var gamePath: DirAccess
-var gameModPath: DirAccess
+var gamePath: DirAccess = DirAccess.open(OS.get_data_dir())
+var gameDataPath: DirAccess = DirAccess.open(OS.get_data_dir())
+var parsedLaunchSettings: Dictionary = {}
+var gameVersion: String = "1.0.0"
+var modSubpath: String = ""
 
 # Define settings values upon program launch
 func _ready():
@@ -10,26 +13,24 @@ func _ready():
 	# Setting up default directories
 	if platform.contains("Windows") || platform.contains("UWP"):
 		var homeDir := DirAccess.open(OS.get_system_dir(OS.SYSTEM_DIR_DESKTOP))
+		if homeDir == null:
+			return
+		
 		homeDir.change_dir("../") # user directory
 		gamePath = DirAccess.open(homeDir.get_current_dir())
-		gameModPath = DirAccess.open(homeDir.get_current_dir())
 	elif platform.contains("macOS") || platform.contains("Linux") || platform.contains("BSD"):
 		var homeDir := DirAccess.open("~") # by default as home dir
 		if homeDir != null:
 			gamePath = DirAccess.open(homeDir.get_current_dir())
-			gameModPath = DirAccess.open(homeDir.get_current_dir())
 		else:
 			gamePath = DirAccess.open(OS.get_data_dir())
-			gameModPath = DirAccess.open(OS.get_data_dir())
+	else:
+		gamePath = DirAccess.open(OS.get_data_dir())
 	
-	# Finding default gameModPath
-	if platform.contains("Windows") || platform.contains("UWP") || platform.contains("macOS"):
-		if gameModPath.dir_exists("Documents/Paradox Interactive/Hearts of Iron IV/mod"):
-			gameModPath.change_dir("Documents/Paradox Interactive/Hearts of Iron IV/mod")
-	elif platform.contains("Linux"):
-		if gameModPath.dir_exists(".local/share/Paradox Interactive/Hearts of Iron IV/mod"):
-			gameModPath.change_dir(".local/share/Paradox Interactive/Hearts of Iron IV/mod")
-		
+	# Checking for null directories
+	if gamePath == null:
+		gamePath = DirAccess.open(OS.get_data_dir())
+		return
 	
 	# Finding game path
 	if platform.contains("Linux"):
@@ -45,4 +46,24 @@ func _ready():
 		# Checking for steam installation
 		if gamePath.dir_exists("Library/Application Support/Steam/steamapps/common/Hearts of Iron IV"):
 			gamePath.change_dir("Library/Application Support/Steam/steamapps/common/Hearts of Iron IV")
+	
+	# Accessing launch settings file
+	var launchSettingsFile: FileAccess
+	if gamePath.file_exists("launcher-settings.json"):
+		launchSettingsFile = FileAccess.open(gamePath.get_current_dir()+"/launcher-settings.json", FileAccess.READ)
+	else:
+		return
+	
+	# Parsing json
+	var parsedJson: Dictionary = JSON.parse_string(launchSettingsFile.get_as_text())
+	if parsedJson == null:
+		return
+	
+	# Getting values from json
+	gameVersion = parsedJson.rawVersion
+	var parsedDataPath: String = parsedJson.gameDataPath
+	parsedDataPath = parsedDataPath.replace("%USER_DOCUMENTS%", OS.get_system_dir(OS.SYSTEM_DIR_DOCUMENTS))
+	gameDataPath = DirAccess.open(parsedDataPath)
+	modSubpath = parsedJson.gameCachePaths[5]
+	parsedLaunchSettings = parsedJson
 	
--- main_menu.gd
+++ main_menu.gd
@@ -1,23 +1,21 @@
 extends Control
 
 
-var modlistVBox: VBoxContainer
+@onready var modlistVBox: VBoxContainer = get_node("%ModlistVBox")
 var newModScene = preload("res://UtilityScenes/mod_element.tscn")
+var newModPopupScene = preload("res://UtilityScenes/new_mod_popup.tscn")
+
+var newModPopup
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Setting window minimum size
 	DisplayServer.window_set_min_size(Vector2i(720, 640))
-	modlistVBox = $Margins/Tabs/Modlist/Mods/List/VBox
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
 
 func _on_new_mod_button_pressed():
-	var newModInstance = newModScene.instantiate()
-	modlistVBox.add_child(newModInstance)
-	pass # Replace with function body.
+	if newModPopup == null:
+		newModPopup = newModPopupScene.instantiate()
+		add_child(newModPopup)
